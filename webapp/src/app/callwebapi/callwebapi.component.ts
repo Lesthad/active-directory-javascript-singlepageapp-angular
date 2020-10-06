6d29787c1e42f7c323304fb0e922d09056dd137e
Miguel Peraza <mperaza@tiempodevelopment.com>
2020-10-06 11:49:56 -0500

    feat(webapp): Add code to call web api from Azure
    Add code to call web api from Azure

--- webapp/src/app/callwebapi/callwebapi.component.ts
+++ webapp/src/app/callwebapi/callwebapi.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { MsalService } from '@azure/msal-angular';
 
 @Component({
   selector: 'app-callwebapi',
@@ -6,10 +8,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./callwebapi.component.css']
 })
 export class CallwebapiComponent implements OnInit {
+  weatherList: any;
 
-  constructor() { }
+  constructor(private authService: MsalService, private http: HttpClient) { }
 
   ngOnInit(): void {
+    this.getWeatherForecast();
+  }
+
+  getWeatherForecast(): void {
+    this.http.get('https://demoazuread.azure-api.net/v1/weatherforecast')
+    .subscribe({
+      next: (data) => {
+        //console.log(`data: ${JSON.stringify(data)}`);
+        this.weatherList = data;
+      },
+      error: (err: any) => {
+        console.log(`Error to get weatherforecast: ${err}`);
+      }
+    });
   }
 
 }
