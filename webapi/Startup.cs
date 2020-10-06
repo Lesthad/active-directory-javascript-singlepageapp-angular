b29206384bf7181380094a319b762ac57541180f
Miguel Peraza <mperaza@tiempodevelopment.com>
2020-10-06 11:29:42 -0500

    feat(Backend): Enable cors and add publish metadata
    Enable cors and add publish metadata

--- webapi/Startup.cs
+++ webapi/Startup.cs
@@ -26,6 +26,11 @@ namespace demoweather
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddCors(options => {
+                options.AddDefaultPolicy(builder => {
+                    builder.WithOrigins("http://localhost:4200");
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -39,6 +44,8 @@ namespace demoweather
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors();
 
             app.UseAuthorization();
 
